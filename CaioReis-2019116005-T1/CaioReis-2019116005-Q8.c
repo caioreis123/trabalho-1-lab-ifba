e2d6ceecaabeabf4f3834e7a8b8fd3c55a6360e0
caioreis123 <reis.caio@gmail.com>
2019-12-07 14:08:40 -0300

    inicializando o tabuleiro

--- CaioReis-2019116005-T1/CaioReis-2019116005-Q8.c
+++ CaioReis-2019116005-T1/CaioReis-2019116005-Q8.c
@@ -1,36 +1,39 @@
 #define TAM 5
+#define BARCO1 2
+#define BARCO2 1
+#define BARCO3 1
 #include <stdio.h>
 
-void exibeTabuleiroProprio(char tabuleiro[TAM][TAM]){
+void exibeTabuleiroProprio(char tabuleiro[TAM][TAM], int jogador){
   int linha, coluna;
 
-  printf(" ");
-  for(coluna=0;coluna<TAM;coluna++) printf("%d", coluna);
+  printf("TABULEIRO DO JOGADOR: %d\n", jogador);
+  printf("  ");
+  for(coluna=0;coluna<TAM;coluna++) printf("%d ", coluna);
+  printf("\n");
   for (linha=0;linha<TAM;linha++){
     printf("%d ", linha);
     for (coluna=0; coluna<TAM; coluna++){
-      printf("%c",tabuleiro[linha][coluna]);
+      printf("%c ",tabuleiro[linha][coluna]);
     }
     printf("\n");
   }
 }
 
-void colocaBarcos(char tabuleiro[TAM][TAM], int tamBarco){
+int colocaBarcos(char tabuleiro[TAM][TAM], int tamBarco){
   int extremidade, linha1, coluna1, linha2, coluna2, menorLinha, menorColuna, cont;
 
   printf("Digite a linha da extremidade 1\n");
   scanf("%d", &linha1);
-  getchar();
+
   printf("Digite a coluna da extremidade 1\n");
   scanf("%d", &coluna1);
-  getchar();
   
   printf("Digite a linha da extremidade 2\n");
   scanf("%d", &linha2);
-  getchar();
+
   printf("Digite a coluna da extremidade 2\n");
   scanf("%d", &coluna2);
-  getchar();
 
   if(linha1<linha2) menorLinha=linha1;
   else menorLinha=linha2;
@@ -54,24 +57,46 @@ void colocaBarcos(char tabuleiro[TAM][TAM], int tamBarco){
         colocaBarcos(tabuleiro, tamBarco);
       }
     }
-  }  
+  }
+
+  if(linha1==linha2){
+    for(cont=0;cont<tamBarco; cont++){
+      tabuleiro[linha1][menorColuna+cont]='N';      
+    }
+    return 1;
+  }
+
+  if(coluna1==coluna2){
+    for(cont=0;cont<tamBarco; cont++){
+      tabuleiro[coluna1][menorLinha+cont]='N';
+      printf("colocou na mesma coluna!\n");      
+    }
+    return 1;
+  }
+
+  printf("Nao eh possivel posicionar o barco dessa forma. Por favor tente novamente\n");
+  colocaBarcos(tabuleiro, tamBarco);
 }
 
-void preencheTabuleiro(char tabuleiro[TAM][TAM]){
-  int a;
+void preencheTabuleiro(char tabuleiro[TAM][TAM], int jogador){
+  int a;  
+  exibeTabuleiroProprio(tabuleiro, jogador);
+  printf("Tamanho do barco a ser colocado: %d\n", BARCO1);
+  colocaBarcos(tabuleiro, BARCO1);
 
-  exibeTabuleiroProprio(tabuleiro);
-  printf("Tamanho do barco a ser colocado: 4\n");
-  colocaBarcos(tabuleiro, 4);
-  exibeTabuleiroProprio(tabuleiro);
-  printf("Tamanho do barco a ser colocado: 3\n");
-  colocaBarcos(tabuleiro, 3);
-  exibeTabuleiroProprio(tabuleiro);
-  for(a=0; a<3; a++){
-    printf("Tamanho do barco a ser colocado: 1\n");
-    colocaBarcos(tabuleiro, 1);
-    exibeTabuleiroProprio(tabuleiro);
-  }
+  // ***********************************comentado só pra facilitar o teste. NÃO APAGUE!!!!!!!!!!!!!!!!!!!!
+
+  // printf("Tabuleiro do jogador %d\n", jogador);
+  // exibeTabuleiroProprio(tabuleiro, jogador);
+  // printf("Tamanho do barco a ser colocado: %d\n", BARCO2);
+  // colocaBarcos(tabuleiro, BARCO2);
+
+  // for(a=0; a<3; a++){
+  //   printf("Tabuleiro do jogador %d\n", jogador);
+  //   exibeTabuleiroProprio(tabuleiro, jogador);
+  //   printf("Tamanho do barco a ser colocado: %d\n", BARCO3);
+  //   colocaBarcos(tabuleiro, BARCO3);
+  // }
 }
 
 void inicializarTabuleiros(char tabuleiro1[TAM][TAM], char tabuleiro2[TAM][TAM]){ 
@@ -87,13 +112,11 @@ void inicializarTabuleiros(char tabuleiro1[TAM][TAM], char tabuleiro2[TAM][TAM])
   int jogador;
   for(jogador=1;jogador<3;jogador++){
     
-    if(jogador==1){
-      printf("Tabuleiro do jogador %d\n", jogador);
-      preencheTabuleiro(tabuleiro1);
+    if(jogador==1){      
+      preencheTabuleiro(tabuleiro1, jogador);
     }
-    else{
-      printf("Tabuleiro do jogador %d\n", jogador);
-      preencheTabuleiro(tabuleiro2);
+    else{      
+      preencheTabuleiro(tabuleiro2, jogador);
     }
   }
 }
@@ -101,6 +124,10 @@ void inicializarTabuleiros(char tabuleiro1[TAM][TAM], char tabuleiro2[TAM][TAM])
 void trocaTurno(int *turno){
   if(*turno==1) *turno=2;
   else *turno=1;
+}
+
+void tiro(char tabuleiro1[TAM][TAM], char tabuleiro2[TAM][TAM], int turno){
+
 }
 
 int main(){
@@ -111,6 +138,7 @@ int main(){
 
   while(continua){
     inicializarTabuleiros(tabuleiro1, tabuleiro2);
+    tiro(tabuleiro1, tabuleiro2, turno);
     trocaTurno(&turno);
   }
 }
